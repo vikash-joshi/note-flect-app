ffba77b162731ef446ab16ff386f6cfba959063c
vikashjoshi <vikashjoshi1651998@gmail.com>
2024-08-17 14:49:06 +0530

    publish fix url code

--- backend/db/db.js
+++ backend/db/db.js
@@ -18,13 +18,10 @@ const uri = "mongodb+srv://dbuser:<n43cuuB2yXOUvGOu>@cluster0.n6mih0d.mongodb.ne
 
 
 mongodb+srv://dbuser:n43cuuB2yXOUvGOu@cluster0.mongodb.net/mydatabase?retryWrites=true&w=majority*/
-
+require('dotenv').config();
+//const mongoURI=process.env.Mongoose_URI;
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const url="mongodb+srv://jitani5571:GL0YqXDoCeY8i0S2@noteapp.qhbis.mongodb.net/?retryWrites=true&w=majority&appName=NoteApp"
-//const uri="mongodb://jitani5571:<GL0YqXDoCeY8i0S2>@<NoteApp>/NoteFlect?ssl=true&replicaSet=atlas-13gsof-shard-0&authSource=admin&retryWrites=true&w=majority&appName=NoteApp"
-const uri = "mongodb+srv://jitani5571:GL0YqXDoCeY8i0S2@NoteApp.mongodb.net/NoteFlect?retryWrites=true&w=majority";
-//const uri = "mongodb+srv://vikash:RX25xpojrKvq66vV@cluster0.mongodb.net/NoteFlect?retryWrites=true&w=majority";
-
+const url=process.env.Mongoose_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(url, {
   serverApi: {
