8530e2dd1b1b476491a522f1b77beee064d8687a
vikashjoshi <vikashjoshi1651998@gmail.com>
2024-08-21 00:32:07 +0530

    Reuqest changes

--- backend/common/Request_Methods.js
+++ backend/common/Request_Methods.js
@@ -2,25 +2,82 @@ const { Request } = require("../models/Request");
 const mongoose = require('mongoose');
 
 
-const GetRequestByUser=async(UserId,UserType)=>{
-    const Condition=UserType == 'Admin' ? {}:{UserReqId:new mongoose.Types.ObjectId(UserId)};
-    let RequestDetail =  await Request.find(Condition).populate('UserReqId', 'name email').exec();
-    return RequestDetail.map((x)=>({
-        Name:x['UserReqId']['name'],
-        Email:x['UserReqId']['email'],
-        RequestType:x['RequestType'],
-        RequestQuery:x['RequestQuery'],
-        RequestTitle:x['RequestTitle'],
-        UserReqId:x['UserReqId'],
-        RequestStatus:x['RequestStatus'],
-        Response:x['Response'],
-        CreatedAt:x['createdAt'],
-        modifiedAt:x['modifiedAt'],
-        _id:['_id']
-    }));
+const GetRequestByUser = async (UserId, UserType) => {
+    console.log(UserType)
+    const Condition = UserType == 'Admin' ? {} : { UserReqId: new mongoose.Types.ObjectId(UserId) };
+    console.log(Condition)
+    let RequestDetail = await Request.find(Condition).populate('UserReqId', 'name email').exec();
+RequestDetail.forEach((x) => ({
+        Name: x['UserReqId']['name'],
+        Email: x['UserReqId']['email'],
+        RequestType: x['RequestType'],
+        RequestQuery: x['RequestQuery'],
+        RequestTitle: x['RequestTitle'],
+        UserReqId: x['UserReqId'],
+        RequestStatus: x['RequestStatus'],
+        Response: x['Response'],
+        CreatedAt: x['createdAt'],
+        modifiedAt: x['modifiedAt'],
+        _id: x['_id']
+    }))
+    return RequestDetail;
 }
 
+const SaveRequest = async (_Request, UserId) => {
+    let _RequestModel, NoteResult;
+    if (!_Request?._id && _Request?._id!='') {
+        NoteResult = await Request.updateOne(
+            {
+                _id: _Request?._id.toString()
+            },
+            {
+                $set: {
+                    Response: _Request?.Response,
+                    RequestStatus: _Request?.RequestStatus,
+                    modifiedAt: Date.now()
+                }
+            }
+        );
+        if (UpdateNote && UpdateNote.modifiedCount > 0) {
+            return {
+                message: "1:Update Successfully",
+                Data: {
+                    id: _ExistingNote.id
+                }
+            };
+        } else {
+            return {
+                message: "0:Update Failed",
+                Data: null
+            };
+        }
+    }
+    else {
+        _RequestModel = new Request({
+            RequestQuery: _Request.RequestQuery,
+            RequestType: _Request.RequestType,
+            RequestTitle: _Request.RequestTitle,
+            UserReqId: UserId,
+            RequestStatus: "In Process",
+            createdAt: Date.now()
+        });
+        NoteResult = await _RequestModel.save();
+        console.log(NoteResult)
+        if (NoteResult) {
+            return {
+                message: "1:Save Success"
+            };
+        } else {
+            return {
+                message: "0:Save Failed",
+                Data: null
+            };
+        }
+    }
 
-module.exports = { GetRequestByUser }
+};
+
+
+module.exports = { GetRequestByUser,SaveRequest }
 
 
--- backend/routes/Request.js
+++ backend/routes/Request.js
@@ -1,12 +1,16 @@
 var express = require("express");
 var router = express.Router();
 const authenticateToken = require("../middleware/fetchuser");
-const { GetRequestByUser } = require("../common/Request_Methods");
-
+const { GetRequestByUser, SaveRequest } = require("../common/Request_Methods");
+const {
+  body,
+  validationResult
+} = require("express-validator");
 router.get('/GetRequestByUser',authenticateToken, async (req,res,next)=>{
     try {
-        const _RequestList = GetRequestByUser(req.body.UserId);
-        if (_RequestList && _RequestList?.length>0) {
+        const _RequestList = await GetRequestByUser(req.body.UserId);
+        console.log('reuqtes',_RequestList)
+        if (_RequestList) {
           res.status(200).json(_RequestList);
         } else {
           res.status(200).json({message: "0:Data Not Found" });
@@ -14,6 +18,33 @@ router.get('/GetRequestByUser',authenticateToken, async (req,res,next)=>{
       } catch (ex) {
         res.status(200).json({message: "0:" + ex.message});
       }
+});
+/*   Name:x['UserReqId']['name'],
+        Email:x['UserReqId']['email'],
+        RequestType:x['RequestType'],
+        RequestQuery:x['RequestQuery'],
+        RequestTitle:x['RequestTitle'],
+        UserReqId:x['UserReqId'],
+        RequestStatus:x['RequestStatus'],
+        Response:x['Response'],
+        CreatedAt:x['createdAt'],
+        modifiedAt:x['modifiedAt'],
+        _id:['_id']*/
+router.post('/Save',[body("RequestType", "Request Type Cannot Be Blank").exists(),
+  body("RequestQuery", "RequestQuery Cannot Be Blank").exists(),
+  body("RequestTitle", "RequestTitle Cannot Be Blank").exists() ],authenticateToken, async (req,res,next)=>{
+  try {
+    const _error = validationResult(req);
+    if (!_error.isEmpty()) {
+        res.status(200).json({ error: _error.array().map((x) => x.path + " " + x.msg)});
+    } else {
+      console.log(req.body)
+     let Result=await SaveRequest(req.body,req.body?.UserId);
+     res.status(200).json(Result) ;
+    }
+    } catch (ex) {
+      res.status(200).json({message: "0:" + ex.message});
+    }
 });
 
 
